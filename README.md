a6162cccf22ec757e564306c80a01cc4f868227d
Andor Saga <andor.saga@gmail.com>
2018-07-15 14:47:15 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Barrel Blast
+
+[play]()
---
+++ src/sketches/test/index.js
@@ -0,0 +1,13 @@
+/*
+  Andor Saga
+  Barrel Blast
+*/
+
+function setup() {
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  setAttributes({ 'antialias': true });
+}
+
+function draw() {
+  background(0);
+}
