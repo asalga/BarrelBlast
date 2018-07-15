4c37e8f2d47f639315cb7fa2acfc282773758b27
Andor Saga <andor.saga@gmail.com>
2018-07-15 15:02:09 -0400

    .

--- src/sketches/test/index.js
+++ src/sketches/test/index.js
@@ -6,8 +6,12 @@
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
   setAttributes({ 'antialias': true });
+  console.log('asdf');
 }
 
 function draw() {
   background(0);
+
+  fill(255,0,0);
+  rect(0,0,40,30);
 }
